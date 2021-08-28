4a30972a947946d4c9fead011cab5d177211b7f7
unknown <jonathums@gmail.com>
2021-08-27 20:23:55 -0500

    Add theme color as rule

--- src/veles/webbuilder/rules.ts
+++ src/veles/webbuilder/rules.ts
@@ -8,17 +8,20 @@ export const BASE_PAGE_RULE = `{{ base }}`;
 export const META_DATA_RULE = `{{ meta }}`;
 export const META_TITLE_RULE = `{{ title }}`;
 export const META_SUBTITLE_RULE = `{{ subtitle }}`;
+export const META_THEME_COLOR_RULE = `{{ theme-color }}`;
 export const META_STATEMENT_RULE = `{{ statement }}`;
 export const META_DESC_RULE = `{{ description }}`;
 
 export let META_TITLE = site["title"];
 export let META_SUBTITLE = site["subtitle"];
+export let META_THEME_COLOR = site["theme-color"]
 export let META_STATEMENT = site["statement"];
 export let META_DESC = site["description"];
 
 export function setMatter(matter: any) {
     META_TITLE = getMatter(matter, "title", site);
     META_SUBTITLE = getMatter(matter, "subtitle", site);
+    META_THEME_COLOR = getMatter(matter, "theme-color", site);
     META_STATEMENT = getMatter(matter, "statement", site);
     META_DESC = getMatter(matter, "description", site);
 }
@@ -26,6 +29,7 @@ export function setMatter(matter: any) {
 export function buildWithMatter(str: string) {
     return str.split(META_TITLE_RULE).join(META_TITLE)
         .split(META_SUBTITLE_RULE).join(META_SUBTITLE)
+        .split(META_THEME_COLOR_RULE).join(META_THEME_COLOR)
         .split(META_STATEMENT_RULE).join(META_STATEMENT)
         .split(META_DESC_RULE).join(META_DESC);
 }
